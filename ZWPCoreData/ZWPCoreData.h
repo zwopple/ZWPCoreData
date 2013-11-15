d341ce2aca0abc0113f417868452ed15f244e700
Robert Payne <robertpayne@me.com>
2013-11-16 00:10:47 +1300

    Added include header

---
+++ ZWPCoreData/ZWPCoreData.h
@@ -0,0 +1,4 @@
+#import <CoreData/CoreData.h>
+#import <ZWPCoreData/NSManagedObject+ZWPCoreData.h>
+#import <ZWPCoreData/NSManagedObjectContext+ZWPCoreData.h>
+#import <ZWPCoreData/NSPersistentStore+ZWPCoreData.h>
